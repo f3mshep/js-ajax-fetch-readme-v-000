77608cd391a9a34736345dd316d9f685ace23aa4
Alexandra <github email address>
2017-11-30 10:15:46 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+const token = '237199e6fd113e31f54adb579164b2a45aab8577'
+// secret code 237199e6fd113e31f54adb579164b2a45aab8577
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
